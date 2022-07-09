281b0e138426b1d36beb71d2f0018fbb8757e1e5
kim04 <kim04129@gmail.com>
2022-07-09 12:00:46 +0900

    22.07.09

---
+++ app/src/main/java/com/hama/habbymaster/view/logo/Logo.kt
@@ -0,0 +1,14 @@
+package com.hama.habbymaster.view.logo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hama.habbymaster.R
+
+class Logo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+    }
+
+    
+}
---
+++ app/src/main/java/com/hama/habbymaster/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hama.habbymaster.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hama.habbymaster.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
